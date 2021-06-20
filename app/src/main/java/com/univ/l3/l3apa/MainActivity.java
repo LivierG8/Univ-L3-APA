889c7b0546713955f793dc543e3ac1198808bc2f
LivierG <12056055+LivierG8@users.noreply.github.com>
2021-06-20 22:49:18 +0200

    Second commit

--- app/src/main/java/com/univ/l3/l3apa/MainActivity.java
+++ app/src/main/java/com/univ/l3/l3apa/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void validationConnexion (View v) {
-        Intent intent = new Intent (this, Generale.class);
-        startActivity(intent);
+        EditText userText = (EditText) findViewById(R.id.editTextUsername);
+        EditText passText = (EditText) findViewById(R.id.editTextPassword);
+
+        if (userText.getText().toString().equals("client") && passText.getText().toString().equals("passe")) {
+            if (type.equals("parcours")) {
+                Intent intent = new Intent (this, Generale.class);
+                startActivity(intent);
+            } else if (type.equals("activite")) {
+                Intent intent = new Intent (this, Activites.class);
+                startActivity(intent);
+            } else {
+                finish();
+            }
+        } else {
+            finish();
+        }
     }
 }
